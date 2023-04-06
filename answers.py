bd5a46aa8effba182031e5fb704e1230c3295eb2
Sarah <sarahpardeike@gmail.com>
2023-04-06 16:23:54 +0200

    exercise done

---
+++ answers.py
@@ -0,0 +1,33 @@
+# Task 1
+
+a = 123
+b = 43.5
+c = 7-3j
+d = "Hallo"
+e = isinstance(1, int)
+print("Task 1\n\n",a,"\n",b,"\n",c,"\n",d,"\n",e,"\n")
+
+# Task 2
+print("Task 2\n")
+print(a, "is type of", type(a))
+print(b, "is type of", type(b))
+print(c, "is type of", type(c))
+print(d, "is type of", type(d))
+print(e, "is type of", type(e))
+
+#Task 3
+print("\nTask 3\n")
+print(isinstance(a, int))
+print(isinstance(b, str))
+print(isinstance(c, complex))
+print(isinstance(d, str))
+print(isinstance(e, str))
+
+#Task 4
+print("\nTask 4\n")
+print("Is", a, "an instance of int?:",isinstance(a, int))
+print("Is", b, "an instance of bool?:",isinstance(b, bool))
+print("Is", c, "an instance of complex?:",isinstance(c, complex))
+print("Is", d, "an instance of bool?:",isinstance(d, bool))
+print("Is", e, "an instance of float?:",isinstance(e, float))
+
