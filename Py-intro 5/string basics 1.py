241e6acec839f35fb01d5c2b22ca2eb944a491be
Nally11 <mythk2@hotmail.com>
2023-04-27 13:48:14 +0200

    okay

---
+++ Py-intro 5/string basics 1.py
@@ -0,0 +1,73 @@
+print("---TASK 1---")
+print()
+
+city = "london"
+
+print(city)
+
+print()
+print("---TASK 2---")
+
+
+print()
+
+city = ("Berlin")
+population = ("3645000")
+print(city + ": " + population)
+
+
+print()
+print("---TASK 3---")
+print()
+
+city = ("London")
+population = ("9000000")
+check = str(city.isalpha())
+print("City:" + " " + city + "(" + check + ")")
+print("Population:" + " " + population)
+
+
+print()
+print("---TASK 4---")
+print()
+
+
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+
+check1 = (text.count("capital"))
+if check1 >0:
+    print(text)
+
+
+print()
+print("---TASK 5---")
+print()
+
+text = "Berlin straddles the banks of the Spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau."
+
+new_text = text.split(" ")
+
+print(new_text)
+
+
+print()
+print("---TASK 6---")
+print()
+
+text = "Bnerlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital"
+
+print(text.replace("capital","capital of Germany"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
